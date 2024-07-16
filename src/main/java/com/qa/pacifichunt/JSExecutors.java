e8ef245f3a28cbc630e148668cd6cdc22487ac96
Rasmi Jati <jatirasmi65@gmail.com>
2024-07-16 18:07:01 +0545

    click on terms of services using JS Executor

--- src/main/java/com/qa/pacifichunt/JSExecutors.java
+++ src/main/java/com/qa/pacifichunt/JSExecutors.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -14,8 +16,9 @@ import org.testng.annotations.Test;
 public class JSExecutors {
 	WebDriver driver;
 	String baseUrl = "https://pacifichunt.com";
+	JavascriptExecutor js;
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void clickPrivacyAndPolicy() throws InterruptedException {
 
 		driver = new ChromeDriver();
@@ -24,22 +27,36 @@ public class JSExecutors {
 		driver.get(baseUrl);
 
 		WebElement privacy = driver.findElement(By.xpath("//a[.='Privacy Policy']/li"));
-		WebElement vritTxtFooter = driver.findElement(By.xpath("//span[.='Vrit Tech']"));
 		Thread.sleep(2000);
 
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].scrollIntoView(true)", vritTxtFooter);
+		js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", privacy);
 		Thread.sleep(3000);
 
 		privacy.click();
-
-		// js.executeScript("arguments[0].click()", privacy);
-		Thread.sleep(2000);
+//		Thread.sleep(2000);
 
 	}
 
+	@Test(enabled = false)
+	public void clickTermsOfServices() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(baseUrl);
+
+		WebElement termOfService = driver.findElement(By.xpath("//li[.='Terms of Service']"));
+		js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", termOfService);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.elementToBeClickable(termOfService));
+		termOfService.click();
+	}
+	
+
 	@AfterMethod
-	public void closeBrowser() {
+	public void closeBrowser() throws InterruptedException {
+		Thread.sleep(2000);
 		Reporter.log("Test Completed", true);
 		driver.quit();
 	}
