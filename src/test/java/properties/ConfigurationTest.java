d8536c1fadb08da3040918ac401ceca097d47de4
Rasmi Jati <jatirasmi65@gmail.com>
2024-07-22 08:12:06 +0545

    properties file

--- src/test/java/properties/ConfigurationTest.java
+++ src/test/java/properties/ConfigurationTest.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -159,7 +158,6 @@ public class ConfigurationTest {
 		return prop.getProperty("email");
 	}
 
-//dd  dp
 	public String getPassword() {
 		return prop.getProperty("password");
 	}
